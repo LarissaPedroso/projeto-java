a1e5d3452ddd66211c901f41526b7b707c55803b
LarissaPedroso <lary_santos2000@hotmail.com>
2023-12-22 11:59:32 -0300

    atualizacoes

---
+++ src/bookstore/books/Secondhand.java
@@ -0,0 +1,10 @@
+package bookstore.books;
+
+public class Secondhand extends Books{
+
+	public Secondhand(int cod, String bookName, String bookAuthor, float price) {
+		super(cod, bookName, bookAuthor, price);
+		// TODO Auto-generated constructor stub
+	}
+
+}
